8718d140326b69a67fe3641912b39d3b19570fba
Azimjon Ilkhomov <ajilkhomov@gmail.com>
2025-01-21 21:52:52 -0500

    Change to absolute path

--- data/data.go
+++ data/data.go
@@ -2,6 +2,8 @@ package data
 
 import (
 	"database/sql"
+	"os"
+	"path/filepath"
 
 	_ "leap/logger"
 
@@ -12,15 +14,23 @@ import (
 var db *sql.DB
 
 func OpenDatabase() error {
-
-	var err error
-
-	db, err = sql.Open("sqlite3", "./.data.db")
+	home, err := os.UserHomeDir()
 	if err != nil {
 		return err
 	}
 
-	log.Debug("Successfully connected to the database!")
+	path := filepath.Join(home, ".leap", "data.db")
+	err = os.MkdirAll(filepath.Dir(path), 0755) // Ensure the directory exists
+	if err != nil {
+		return err
+	}
+
+	db, err = sql.Open("sqlite3", path)
+	if err != nil {
+		return err
+	}
+
+	log.Debug("Successfully connected to the database!", "path", path)
 
 	return db.Ping()
 }
